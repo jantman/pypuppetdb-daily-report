279971259fc9451571248d1b014e5798268af920
Jason Antman <Jason.Antman@coxinc.com>
2014-06-11 07:40:21 -0400

    tests for console_entry_point

--- pypuppetdb_daily_report/tests/pdr_test.py
+++ pypuppetdb_daily_report/tests/pdr_test.py
@@ -29,6 +29,8 @@ import pytest
 import sys
 import os
 import shutil
+import mock
+import logging
 
 from pypuppetdb_daily_report import pypuppetdb_daily_report as pdr
 
@@ -48,7 +50,7 @@ class OptionsObject(object):
         self.num_days = 7
 
 
-class TestOptions:
+class Test_parse_args:
     """
     Tests the CLI option/argument handling
     """
@@ -75,7 +77,7 @@ class TestOptions:
 
     def test_verbose(self):
         """
-        Test the parse_opts option parsing method with verbose specified
+        Test the parse_args option parsing method with verbose specified
         """
         argv = ['pypuppetdb_daily_report', '-v']
         x = pdr.parse_args(argv)
@@ -83,7 +85,7 @@ class TestOptions:
 
     def test_debug(self):
         """
-        Test the parse_opts option parsing method with debug specified
+        Test the parse_args option parsing method with debug specified
         """
         argv = ['pypuppetdb_daily_report', '-vv']
         x = pdr.parse_args(argv)
@@ -91,7 +93,7 @@ class TestOptions:
 
     def test_host(self):
         """
-        Test the parse_opts option parsing method with a PuppetDB URL
+        Test the parse_args option parsing method with a PuppetDB URL
         """
         argv = ['pypuppetdb_daily_report', '-p', 'foobar']
         x = pdr.parse_args(argv)
@@ -102,8 +104,84 @@ class TestOptions:
 
     def test_num_days(self):
         """
-        Test the parse_opts option parsing method with number of days specified
+        Test the parse_args option parsing method with number of days specified
         """
         argv = ['pypuppetdb_daily_report', '-n', '14']
         x = pdr.parse_args(argv)
         assert x.num_days == 14
+
+
+class Test_console_entry_point:
+    """ test console_entry_point """
+
+    def test_defaults(self):
+        """ with default values """
+        parse_args_mock = mock.MagicMock()
+        opts_o = OptionsObject()
+        opts_o.host = 'foobar'
+        parse_args_mock.return_value = opts_o
+
+        main_mock = mock.MagicMock()
+
+        with mock.patch('pypuppetdb_daily_report.pypuppetdb_daily_report.parse_args', parse_args_mock):
+            with mock.patch('pypuppetdb_daily_report.pypuppetdb_daily_report.main', main_mock):
+                pdr.console_entry_point()
+        assert parse_args_mock.call_count == 1
+        assert main_mock.call_count == 1
+
+    def test_nohost(self):
+        """ without a host specified """
+        parse_args_mock = mock.MagicMock()
+        opts_o = OptionsObject()
+        parse_args_mock.return_value = opts_o
+        main_mock = mock.MagicMock()
+
+        with mock.patch('pypuppetdb_daily_report.pypuppetdb_daily_report.parse_args', parse_args_mock):
+            with mock.patch('pypuppetdb_daily_report.pypuppetdb_daily_report.main', main_mock):
+                with pytest.raises(SystemExit) as excinfo:
+                    pdr.console_entry_point()
+        assert parse_args_mock.call_count == 1
+        assert main_mock.call_count == 0
+        assert excinfo.value.message == "ERROR: you must specify the PuppetDB hostname with -p|--puppetdb"
+
+    def test_verbose(self):
+        """ with -v """
+        parse_args_mock = mock.MagicMock()
+        opts_o = OptionsObject()
+        opts_o.host = 'foobar'
+        opts_o.verbose = 1
+        parse_args_mock.return_value = opts_o
+
+        logger_mock = mock.MagicMock()
+
+        main_mock = mock.MagicMock()
+
+        with mock.patch('pypuppetdb_daily_report.pypuppetdb_daily_report.parse_args', parse_args_mock):
+            with mock.patch('pypuppetdb_daily_report.pypuppetdb_daily_report.main', main_mock):
+                with mock.patch('pypuppetdb_daily_report.pypuppetdb_daily_report.logger', logger_mock):
+                    pdr.console_entry_point()
+        assert parse_args_mock.call_count == 1
+        assert main_mock.call_count == 1
+        assert logger_mock.setLevel.call_count == 1
+        assert logger_mock.setLevel.call_args == mock.call(logging.INFO)
+
+    def test_debug(self):
+        """ with -v """
+        parse_args_mock = mock.MagicMock()
+        opts_o = OptionsObject()
+        opts_o.host = 'foobar'
+        opts_o.verbose = 2
+        parse_args_mock.return_value = opts_o
+
+        logger_mock = mock.MagicMock()
+
+        main_mock = mock.MagicMock()
+
+        with mock.patch('pypuppetdb_daily_report.pypuppetdb_daily_report.parse_args', parse_args_mock):
+            with mock.patch('pypuppetdb_daily_report.pypuppetdb_daily_report.main', main_mock):
+                with mock.patch('pypuppetdb_daily_report.pypuppetdb_daily_report.logger', logger_mock):
+                    pdr.console_entry_point()
+        assert parse_args_mock.call_count == 1
+        assert main_mock.call_count == 1
+        assert logger_mock.setLevel.call_count == 1
+        assert logger_mock.setLevel.call_args == mock.call(logging.DEBUG)
